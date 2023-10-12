a943732aff3ac01c4532aafd7b67287ca1846c7b
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2023-10-12 12:55:46 +0530

    done

--- assets/js/add.js
+++ assets/js/add.js
@@ -88,3 +88,50 @@ Contact.addEventListener("click", function () {
     cross6.classList.toggle("cross-3");
 });
 
+// slide
+$('.slider-for').slick({
+    arrow: false,
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider-nav',
+    responsive: [{
+            breakpoint: 991.5,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 767.5,
+            settings: {
+                centerMode: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 575.5,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+$('.slider-nav').slick({
+    arrow: false,
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    focusOnSelect: true
+});
+
+$('a[data-slide]').click(function(e) {
+    e.preventDefault();
+    var slide = $(this).data('slide');
+    $('.slider-nav').slick('slickGoTo', slide - 1);
+});

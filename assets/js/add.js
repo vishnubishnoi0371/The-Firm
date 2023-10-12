a981126af85b41ed50c3da25d56d58c6b15bb93c
abamal <abamal@uoguelph.ca>
2023-10-12 10:54:23 +0530

    Js no effect change

--- assets/js/add.js
+++ assets/js/add.js
@@ -148,3 +148,36 @@ $('.section-7-slider2').slick({
     pauseOnHover: true,
     pauseOnDotsHover: true
 });
+
+// NAVBAR 
+let label = document.querySelector(".label");
+let menu = document.querySelector("#menu");
+let open = 0;
+let aTags = document.querySelectorAll("#menu li a");
+
+aTags.forEach((c) => {
+    c.addEventListener("click", () => {
+        if (open != 0) {
+            menu.style.right = '-100%';
+            document.body.classList.remove("overflow-y-hidden");
+            label.classList.remove("label-switch");
+            open--;
+        }
+    })
+})
+
+label.addEventListener("click", () => {
+    if (open == 0) {
+        menu.style.right = 0;
+        document.body.classList.add("overflow-y-hidden");
+        label.classList.add("label-switch");
+        open++;
+    }
+
+    else {
+        menu.style.right = '-100%';
+        document.body.classList.remove("overflow-y-hidden");
+        label.classList.remove("label-switch");
+        open--;
+    }
+});

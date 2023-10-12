09740bb355f4614b71d3c48f6e67d0c1282e1f42
abamal <abamal@uoguelph.ca>
2023-10-12 14:05:35 +0530

    added to-top in page 4 and 5

--- assets/Extra Pages/page-4,5/assets/js/add.js
+++ assets/Extra Pages/page-4,5/assets/js/add.js
@@ -66,3 +66,14 @@ Contact.addEventListener("click",function(){
     cross5.classList.toggle("cross-2");
     cross6.classList.toggle("cross-3");
 });
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        toTop.style.display = "flex";
+    }
+    else {
+        toTop.style.display = "none";
+    }
+});
